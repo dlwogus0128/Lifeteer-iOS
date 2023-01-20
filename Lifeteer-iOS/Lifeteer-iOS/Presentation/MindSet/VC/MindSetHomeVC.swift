5885fb32a6a9a027c473011dfa6c9e698d73ce4e
dlwogus0128 <79050615+dlwogus0128@users.noreply.github.com>
2023-01-20 12:30:39 +0900

    [Feat] #8 - 네비 바 구현

--- Lifeteer-iOS/Lifeteer-iOS/Presentation/MindSet/VC/MindSetHomeVC.swift
+++ Lifeteer-iOS/Lifeteer-iOS/Presentation/MindSet/VC/MindSetHomeVC.swift
@@ -12,8 +12,29 @@ import Then
 
 final class MindSetHomeVC: UIViewController {
 
+    // MARK: - UI Components
+    
+    private lazy var naviBar = CustomNavigationBar(self, type: .menuButtonWithBI)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUI()
+        setLayout()
+    }
+}
+
+// MARK: - UI & Layout
+
+extension MindSetHomeVC {
+    private func setUI() {
         view.backgroundColor = .mainBackground
+    }
+
+    private func setLayout() {
+        view.addSubviews(naviBar)
+        
+        naviBar.snp.makeConstraints { make in
+            make.leading.top.trailing.equalTo(view.safeAreaLayoutGuide)
+        }
     }
 }
