5752ad214ba72ba55276138763c7a7cf8153cb33
dlwogus0128 <79050615+dlwogus0128@users.noreply.github.com>
2023-03-09 01:22:14 +0900

    [Feat] #13 - PageViewController 레이아웃 잡기

--- Lifeteer-iOS/Lifeteer-iOS/Presentation/Landing/VC/DetailLandingPageVC.swift
+++ Lifeteer-iOS/Lifeteer-iOS/Presentation/Landing/VC/DetailLandingPageVC.swift
@@ -15,12 +15,32 @@ final class DetailLandingPageVC: UIViewController {
     // MARK: - UI Components
     
     private lazy var naviBar = CustomNavigationBar(self, type: .backButton)
-    private let detailLandingPageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal)
+    private lazy var pageDataViewControllers: [UIViewController] = {
+        return [FirstDetailLandingPageVC(), SecondDetailLandingPageVC(), ThirdDetailLandingPageVC(), LastDetailLandingPageVC()]
+    }()
+    private let detailLandingPageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setUI()
         setLayout()
+        setFirstDetailLandingPageVC()
+        setDelegate()
+    }
+}
+
+// MARK: - Methods
+
+extension DetailLandingPageVC {
+    private func setFirstDetailLandingPageVC() {
+        if let firstVC = pageDataViewControllers.first {
+            detailLandingPageViewController.setViewControllers([firstVC], direction: .forward, animated: true, completion: nil)
+        }
+    }
+    
+    private func setDelegate() {
+        detailLandingPageViewController.delegate = self
+        detailLandingPageViewController.dataSource = self
     }
 }
 
@@ -29,14 +49,49 @@ final class DetailLandingPageVC: UIViewController {
 extension DetailLandingPageVC {
     private func setUI() {
         self.view.backgroundColor = .white
+        self.detailLandingPageViewController.view.backgroundColor = .mainBlack
     }
     
     private func setLayout() {
-        view.addSubviews(naviBar)
+        view.addSubview(naviBar)
+        addChild(detailLandingPageViewController)
+        view.addSubview(detailLandingPageViewController.view)
         
         naviBar.snp.makeConstraints { make in
             make.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
             make.height.equalTo(48)
         }
+        
+        detailLandingPageViewController.view.snp.makeConstraints { make in
+            make.top.equalTo(naviBar.snp.bottom).offset(66)
+            make.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            make.bottom.equalTo(view.safeAreaLayoutGuide).inset(165)
+        }
+        detailLandingPageViewController.didMove(toParent: self)
+    }
+}
+
+// MARK: - UIPageViewControllerDataSource, UIPageViewControllerDelegate
+
+extension DetailLandingPageVC: UIPageViewControllerDataSource, UIPageViewControllerDelegate {
+    
+    /// 왼쪽에서 오른쪽으로 스와이프하기 직전에 호출
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let index = pageDataViewControllers.firstIndex(of: viewController) else {return nil}
+        let previousIndex = index - 1
+        if previousIndex < 0 {
+            return nil
+        }
+        return pageDataViewControllers[previousIndex]
+    }
+    
+    /// 오른쪽에서 왼쪽으로 스와이프하기 직전에 호출
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let index = pageDataViewControllers.firstIndex(of: viewController) else {return nil}
+        let nextIndex = index + 1
+        if nextIndex == pageDataViewControllers.count {
+            return nil
+        }
+        return pageDataViewControllers[nextIndex]
     }
 }
--- Lifeteer-iOS/Lifeteer-iOS/Presentation/Landing/VC/LastDetailLandingPageVC.swift
+++ Lifeteer-iOS/Lifeteer-iOS/Presentation/Landing/VC/LastDetailLandingPageVC.swift
@@ -11,7 +11,7 @@ class LastDetailLandingPageVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.backgroundColor = .magenta
         // Do any additional setup after loading the view.
     }
     
