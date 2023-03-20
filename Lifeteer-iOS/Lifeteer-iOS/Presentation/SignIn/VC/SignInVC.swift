d040bed8c2b0297bce42cc1a55bc42f7dc0c3ba6
dlwogus0128 <79050615+dlwogus0128@users.noreply.github.com>
2023-03-20 15:51:32 +0900

    [Feat] #17 - SingInVC 파일 생성

---
+++ Lifeteer-iOS/Lifeteer-iOS/Presentation/SignIn/VC/SignInVC.swift
@@ -0,0 +1,32 @@
+//
+//  SignInVC.swift
+//  Lifeteer-iOS
+//
+//  Created by 몽이 누나 on 2023/03/20.
+//
+
+import UIKit
+
+import Then
+import SnapKit
+
+final class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUI()
+        setLayout()
+    }
+}
+
+// MARK: - UI & Layout
+
+extension SignInVC {
+    private func setUI() {
+        view.backgroundColor = .white
+    }
+    
+    private func setLayout() {
+        
+    }
+}
